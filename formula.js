ebb2127e221dd1561a9e846edf733abce54a9fa9
rm3d0nc4 <romeromendonca22@gmail.com>
2022-03-24 22:53:55 -0300

    Criando a fórmula

---
+++ formula.js
@@ -0,0 +1,11 @@
+import {print} from './io_utils'
+import {input} from './io_utils.'
+
+function main(){
+    const incial = Number(input('Digite o número inicial:'))
+    const final = Number(input('Digite o número inicial:'))
+    const resultado = (final+incial) * (final/2)
+    print(resultado)
+}
+
+main()
