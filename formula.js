d5199ed5829f51ba74d39eea4b7461f1900e0150
rm3d0nc4 <romeromendonca22@gmail.com>
2022-03-24 22:59:26 -0300

    Formatando mensagem de resultado

--- formula.js
+++ formula.js
@@ -1,11 +1,11 @@
-import {print} from './io_utils'
-import {input} from './io_utils.'
+import {print} from './io_utils.js'
+import {input} from './io_utils.js'
 
 function main(){
     const incial = Number(input('Digite o número inicial:'))
     const final = Number(input('Digite o número inicial:'))
     const resultado = (final+incial) * (final/2)
-    print(resultado)
+    print(`Soma: ${resultado}`)
 }
 
 main()
