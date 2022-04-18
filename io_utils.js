b02ce6d804cf843c81053d26212119771e85f816
Romero <romeromendonca22@gmail.com>
2022-04-17 21:27:43 -0300

    Criando funções para ajudar na leitura dos números e datas

--- io_utils.js
+++ io_utils.js
@@ -7,3 +7,22 @@ export const input = prompt({
 export function print(mensagem, ...optionalParams){
     console.log(mensagem, ...optionalParams)
 }
+
+// Função para auxiliar na leitura dos números
+export function ler_numeros_tipo1(quantidade, tipo = 'valor(es)') {
+    const numero = input(`Digite o valor de ${quantidade} ${tipo}:`).split(' ').map(Number)
+    return numero
+}
+
+// Função para auxiliar na leitura dos números
+export function ler_numeros_tipo2(o_que) {
+        const numero = input(`Digite ${o_que}:`).split(' ').map(Number)
+        return numero
+}
+
+
+// Função para auxiliar na leitura das datas
+export function ler_data( de_que = ' '){
+const data = input(`Digite uma data ${de_que} (dd/mm/aaaa):`).split('/').map(Number)
+return data
+}
