2e59bf610faa3e85fdae9436788ba881777315ac
Romero <romeromendonca22@gmail.com>
2022-05-08 16:10:22 -0300

    Comentando as funções básicas do Turtle

--- turtle/turtle.js
+++ turtle/turtle.js
@@ -1,39 +1,26 @@
 /// <reference path="./lib/Intellisense/js-turtle_hy.ts" />
 //DOCUMENTATION: https://hanumanum.github.io/js-turtle/
 /*
-showGrid(20);      
-forward(distance)  
-right(angle)       
-left(angle) 	   
-goto(x,y) 	       
-clear() 	       
-penup() 	       
-pendown() 	       
-reset() 	       
+showGrid(20); //Escala da malha
+setSpeed(200); // Velocidade do ponteiro     
+forward(distance)  // Desenhar a linha
+right(angle) // Girar (graus) a direita
+left(angle) // Girar (graus) a esquerda
+goto(x,y) // Ir para um ponto (x,y) específico
+clear() // Limpar Desenho	       
+penup() // Não desenhar	       
+pendown() // Voltar a desenhar (finalizar penup())       
+strangeLine(250) // Desenhar a linha em um formato aleatório (rasbiscos)
+// strangeCircle(diametro) // Desenha um círculo com rabiscos
+// strangeSquare(area,intervalo)// Desenhar quadrado padronizado
+reset()
 angle(angle)	   
-width(width)       
+width(width) // Largura da linha      
 
+// Cores da linha
 color(r,g,b)
 color([r,g,b])
 color("red")
 color("#ff0000")
 */
 
-
-showGrid(50); 
-setSpeed(200);
-
-width(5);
-color("blue");
-forward(120);
-
-width(1);
-left(65);
-color("red");
-forward(150);
-
-goto(184,-208);
-width(2);
-right(100);
-color("green");
-forward(150);
