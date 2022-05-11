32ea2e84bd3f2a56452e7324c9cd7e10dd373c7a
Romero Antonio Ramos de Mendonça <romeromendonca22@gmail.com>
2022-05-11 12:58:44 -0300

    #Bee03 - 8/10 concluídas

--- beecrowd/bc_1131.js
+++ beecrowd/bc_1131.js
@@ -8,15 +8,54 @@ const input = fs.readFileSync('bc_1131_input.txt', 'utf8');
 const lines = input.split('\n');
 
 function main() {
-    let controller = 0
-    const grenal = lines[controller].split(' ').map(Number)
-
+    let controller_while = 1
+    let index_lines = 0
     let inter = 0
     let gremio = 0
+    let count_grenais =0
+    let empates = 0
 
-    while(grenal.length1!=1 && grenal[controller]===2) {
+    while( controller_while !=2 ) {
+        const grenal = lines[index_lines].split(' ').map(Number)
 
+        if(grenal[0]>grenal[1]) {
+            inter++
+        } else if(grenal[0]<grenal[1]) {
+            gremio++
+        } else {
+            inter++
+            gremio++
+            empates++
+        }
+        count_grenais++
+
+        console.log('Novo grenal (1-sim 2-nao)')
+
+        index_lines++
+
+        controller_while = lines[index_lines]
+
+        index_lines++
+        
+        
     }
+
+    console.log(`${count_grenais} grenais`)
+    console.log(`Inter:${inter}`)
+    console.log(`Gremio:${gremio}`)
+    console.log(`Empates:${empates}`)
+
+    if (inter>gremio) {
+        console.log('Inter venceu mais')
+    } else if(inter<gremio) {
+        console.log('Gremio venceu mais')
+    } else {
+        console.log('Nao houve vencedor')
+    }
+
+   
 }
+
+
 
 main()
