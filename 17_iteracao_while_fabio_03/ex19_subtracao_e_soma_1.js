5d4930da8ad027b2054e95b2dc16811b4192620c
Romero <romeromendonca22@gmail.com>
2022-06-19 23:08:38 -0300

    17_iteracao_while_fabio_03: editando ex19

--- 17_iteracao_while_fabio_03/ex19_subtracao_e_soma_1.js
+++ 17_iteracao_while_fabio_03/ex19_subtracao_e_soma_1.js
@@ -1,24 +1,37 @@
-import {ler_numeros} from '../io_utils.js'
+import {input, ler_numeros} from '../io_utils.js'
 
 function main() {
+    
+    const n = Number(input('Número: '))
 
-    const [n] = ler_numeros('um número')
     let contador1 = 1
     let contador2 = n
-    let resultado = 0
+    
+    let solucao = 0
+    
+    let calculo
+    
+    while(contador1 <= n) {
+        
+        if(!eh_par(contador2)){
+            calculo = contador2/contador1
 
-    while(contador1<=n){
-        resultado+=(contador1/contador2)
-        contador1++
-        contador2--
+            solucao += calculo
+        } else {
+            
+            calculo = contador1/contador2
+            solucao += calculo
+            console.log(`${contador2}/${contador1}`)
+        }
 
-        resultado-=(contador2/contador1)
         contador1++
         contador2--
     }
 
-    console.log(`Resultado: ${resultado}`)
+    console.log(`Solução: ${solucao}`)
 
 }
+
+const eh_par = n => n % 2 === 0
 
 main()
