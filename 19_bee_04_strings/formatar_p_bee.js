37e39a88ef739c113b3be0f055364c0c1659bcd3
Romero Antonio Ramos de Mendonça <romeromendonca22@gmail.com>
2022-06-23 12:35:08 -0300

    19_bee_04_strings: exercicio 1262; Adicionando funções úteis ao arquivo array_utils.js

--- 19_bee_04_strings/formatar_p_bee.js
+++ 19_bee_04_strings/formatar_p_bee.js
@@ -1,20 +1,72 @@
 import fs from 'fs';
-const input = fs.readFileSync('1276_faixa_de_letras_input', 'utf8');
+const input = fs.readFileSync('1262_leitura_multipla_input', 'utf8');
 
 // Descomentar linha abaixo no Beecrowd
 // const input = require('fs').readFileSync('/dev/stdin', 'utf8');
 const lines = input.split('\n')
 
 function main() {
-    
-    while(lines.length > 1){
+    let contador = 0
 
-        const texto = lines.shift()
-        const faixa = get_faixa_de_letras(texto)
-    
-        const saida = faixa.split(' ').join(', ')
-        console.log(saida)
+    while(!EOF(lines[contador])){
+
+        const processos = lines[contador]
+        contador++
+        const qtd_leituras_simutaneas = lines[contador]
+
+        let qtd_leitura
+        let qtd_ciclos = 0
+        let qtd_processos_leitura = 0
+        let qtd_processos_escrita = 0
+
+        qtd_processos_escrita = contar_elemento('W', processos)
+        qtd_ciclos += qtd_processos_escrita
+        
+        const leituras = remover_elementos_vazios(processos.split('W'))
+        
+        for( let leitura of leituras) {
+            if(leitura.length > qtd_leituras_simutaneas) {
+
+                qtd_leitura = Math.ceil(leitura.length / qtd_leituras_simutaneas)
+                qtd_processos_leitura += qtd_leitura
+
+            } else {
+                qtd_processos_leitura++
+            }
+        }
+
+        qtd_ciclos += qtd_processos_leitura
+
+        console.log(qtd_ciclos)
+        contador++
     }
+    
+}
+
+const EOF = item => item === ''
+
+function remover_elementos_vazios(vetor) {
+    let novo_vetor = new Array()
+
+    for(let elemento of vetor) {
+        if(elemento !== '') {
+            novo_vetor[novo_vetor.length] = elemento
+        }
+    }
+
+    return novo_vetor
+}
+
+function contar_elemento(elemento, string){
+    let qtd = 0
+
+    for(let indice = 0; indice < string.length; indice++){
+        
+        if(string[indice] === elemento){
+            qtd++
+        }
+    }
+    return qtd
 }
 
 main()
